b544feff7f407a0e415c8a18ebf7d4d3fd44b4ac
HelenB84 <helen.boomsluiter@outlook.com>
2021-01-19 10:48:00 +0100

    program interaction loginform

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 
 export default function LogInform (){
-    return <form>
+    const { register, handleSubmit } = useForm();
+
+    function onSubmit(data){
+        console.log(data)
+    }
+
+    return <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="userName">Username</label>
-        <input name={"userName"} type="text"/>
+        <input name={"userName"} type="text" ref={register}/>
         <label htmlFor="password">Password</label>
-        <input name={"password"} type="password"/>
+        <input name={"password"} type="password" ref={register}/>
         <input type="submit"/>
     </form>
 }
